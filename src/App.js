ac2d6dbba2493d481298d785d9396ab3a01d2406
Mhkazzaz <mhkazazz@hotmail.com>
2021-07-05 22:08:33 +1000

    Event Listeners Added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { useState, useRef, useEffect } from 'react';
 import { Card, CardHeader, CardBody } from 'react-simple-card';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.scss';
-import ReactMarkdown from 'react-markdown'
 import { FaExpandArrowsAlt, FaCompressArrowsAlt } from 'react-icons/fa';
-
+import Markdown from 'markdown-to-jsx';
+import ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm';
 
 function App() {
-
-  const [text, setText] = useState('')
+  const initialText = "![ReactJS Logo](https://res.cloudinary.com/practicaldev/image/fetch/s--54ca_F2q--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/1wwdyw5de8avrdkgtz5n.png)"
+  const [text, setText] = useState(initialText)
   const [editorIcon, setEditorIcon] = useState(<FaExpandArrowsAlt />)
   const [previewerIcon, setPreviewerIcon] = useState(<FaExpandArrowsAlt />)
   const [editorCheck, setEditorCheck] = useState(false)
@@ -17,59 +18,49 @@ function App() {
   const previewerCard = useRef();
   const editorInput = useRef();
   const previewerInput = useRef();
+  const editorText = useRef();
+  const previewerText = useRef();
 
   const textChange = (e) => setText(e.target.value)
   
-  // const handleEditorChange = () => {
-  //   if(!editorCheck) {
-  //   editorCard.current.style.height = "100vh"
-  //   previewerCard.current.style.display = "none"
-  //   setEditorIcon(<FaCompressArrowsAlt />)
-  //   } else {
-  //     editorCard.current.style.height = "100px"
-  //     previewerCard.current.style.display = "block"
-  //     setEditorIcon(<FaExpandArrowsAlt />)
-  //   }
+  const handleEditorResize = () => {
+    if(!editorCheck) {
+    editorText.current.style.height = "70vh"
+    previewerCard.current.style.display = "none"
+    setEditorIcon(<FaCompressArrowsAlt />)
+    } else {
+      editorText.current.style.height = "100px"
+      previewerCard.current.style.display = "block"
+      setEditorIcon(<FaExpandArrowsAlt />)
+    }
 
-  //   setEditorCheck(!editorCheck)
-  // }
-
-  // useEffect(() => {
-  //   editorInput.current.addEventListener('onChange', handleEditorChange)
-  // })
-  
-
-  // const handlePreviewerChange = () => {
-  //   if(!previewerCheck) {
-  //     editorCard.current.style.display = "none"
-  //     setPreviewerIcon(<FaCompressArrowsAlt />)
-  //   } else {
-  //     editorCard.current.style.display = "block"
-  //     setPreviewerIcon(<FaExpandArrowsAlt />)
-  //   }
-
-  //   setPreviewerCheck(!previewerCheck)
-  // }
-
-  // useEffect(() => {
-  //   previewerInput.current.addEventListener('onChange', handlePreviewerChange)
-  // })
-
-  const handleClick = () => {
-    console.log("Clicked")
+    setEditorCheck(!editorCheck)
   }
 
+  const handlePreviewerResize = () => {
+    if(!previewerCheck) {
+      previewerText.current.style.minHeight = "100vh"
+      editorCard.current.style.display = "none"
+      setPreviewerIcon(<FaCompressArrowsAlt />)
+    } else {
+      previewerText.current.style.minHeight = "20vh"
+      editorCard.current.style.display = "block"
+      setPreviewerIcon(<FaExpandArrowsAlt />)
+    }
+
+    setPreviewerCheck(!previewerCheck)
+  }
+  
   return(
     <div id="container">
       <div ref={editorCard} id="editor">
           <Card className="card">
             <CardHeader className="header">
               Editor
-            <span><FaExpandArrowsAlt onClick={handleClick} class="icon" ref={previewerInput} /></span>
-              {/* <input ref={editorInput} type="checkbox" checked={editorCheck} /> */}
+            <span onClick={handleEditorResize} className="icon" ref={editorInput}>{editorIcon}</span>
             </CardHeader>
             {/* <CardBody>  */}
-              <textarea id="editor-text" rows="5" cols="75" className="text-area" value={text} onChange={textChange} />
+          <textarea ref={editorText} className="text-area" id="editor-text" rows="5" cols="75" value={text} onChange={textChange} />
             {/* </CardBody> */}
           </Card>
       </div>
@@ -77,11 +68,12 @@ function App() {
         <Card className="card">
           <CardHeader className="header">
             Previewer
-            <span><FaExpandArrowsAlt onClick={handleClick} class="icon" ref={previewerInput} /></span>
-          {/* <input  type="checkbox" checked={previewerCheck} /> */}
+            <span onClick={handlePreviewerResize} className="icon" ref={previewerInput}>{previewerIcon}</span>
            </CardHeader>
            {/* <CardBody> */}
-             <ReactMarkdown id="markdown" className="text-area">{text}</ReactMarkdown>
+          <span ref={previewerText} id="markdown" className="text-area" >
+            <Markdown>{text}</Markdown>
+          </span>
            {/* </CardBody> */}
         </Card>
       </div>
